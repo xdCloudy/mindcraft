8eeac913876c6855859d0484836d8f07938af287
Cloudy <52116030+xdCloudy@users.noreply.github.com>
2026-08-17 22:37:53 +0100

    ci: lint runtime code with Node and browser globals

--- eslint.config.js
+++ eslint.config.js
@@ -5,27 +5,27 @@ import noFloatingPromise from "eslint-plugin-no-floating-promise";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  // First, import the recommended configuration
   pluginJs.configs.recommended,
-
-  // Then override or customize specific rules
   {
     plugins: {
       "no-floating-promise": noFloatingPromise,
     },
     languageOptions: {
-      globals: globals.browser,
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
       ecmaVersion: 2021,
       sourceType: "module",
     },
     rules: {
-      "no-undef": "error",              // Disallow the use of undeclared variables or functions.
-      "semi": ["error", "always"],      // Require the use of semicolons at the end of statements.
-      "curly": "off",                   // Do not enforce the use of curly braces around blocks of code.
-      "no-unused-vars": "off",          // Disable warnings for unused variables.
-      "no-unreachable": "off",          // Disable warnings for unreachable code.
-      "require-await": "error",         // Disallow async functions which have no await expression
-      "no-floating-promise/no-floating-promise": "error", // Disallow Promises without error handling or awaiting
+      "no-undef": "error",
+      "semi": ["error", "always"],
+      "curly": "off",
+      "no-unused-vars": "off",
+      "no-unreachable": "off",
+      "require-await": "error",
+      "no-floating-promise/no-floating-promise": "error",
     },
   },
 ];
